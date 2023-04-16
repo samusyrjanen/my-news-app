99c9351b52589739deac3da018625d94463d4f10
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-04-16 18:14:08 +0300

    nothing new

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ import json
 
 load_dotenv()
 
-"""
-API needs to have
-- free
-- full text content
-- publish date range is wide enough
-"""
 api_key=os.getenv('API_KEY')
 
-query = 'ukraine'
-url = f'https://newsdata.io/api/1/news?apikey={api_key}&q={query}&language=en'
+query = 'ukraina'
+url = f'https://newsdata.io/api/1/news?apikey={api_key}&q={query}&domain=hs,is'
 
 response = requests.get(url)
 print(response)
@@ -24,9 +18,8 @@ if response.status_code == 200:
     articles = data['results']
     for article in articles:
         print(article['title'])
-        print(article['country'])
+        print(article['link'])
         print(article['pubDate'])
         print()
-    print(articles[0]['content'])
 else:
     print(f"Error: {response.status_code}")
