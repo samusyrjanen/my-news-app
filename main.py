199a7898b79eafaa573354a2716f4e58a60ac339
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-04-16 23:34:34 +0300

    newsapi works

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ import requests
 import json
 
 load_dotenv()
-
-api_key=os.getenv('API_KEY')
+#newsdata_key=os.getenv('NEWSDATA.IO_API_KEY')
+newsapi_key=os.getenv('NEWSAPI.ORG_API_KEY')
 
 query = 'ukraina'
-url = f'https://newsdata.io/api/1/news?apikey={api_key}&q={query}&domain=hs,is'
+#newsdata_url = f'https://newsdata.io/api/1/news?apikey={newsdata_key}&q={query}&domain=hs,is'
+newsapi_url = f'https://newsapi.org/v2/everything?q={query}&domains=yle.fi&apiKey={newsapi_key}'
 
-response = requests.get(url)
-print(response)
+response = requests.get(newsapi_url)
 
 if response.status_code == 200:
     data = json.loads(response.text)
-    articles = data['results']
-    for article in articles:
+    for article in data['articles']:
+        print(article['source'])
         print(article['title'])
-        print(article['link'])
-        print(article['pubDate'])
+        print(article['url'])
+        print(article['publishedAt'])
         print()
+    print(len(data['articles']))
+    print(data['totalResults'])
+
+    # articles = data['results']#newsdata
+    # print(articles)
+    # for article in articles:
+    #     print(article['title'])
+    #     print(article['link'])
+    #     print(article['pubDate'])
+    #     print()
 else:
     print(f"Error: {response.status_code}")
